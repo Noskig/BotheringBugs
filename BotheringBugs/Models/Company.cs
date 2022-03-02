954586dd6e3d1d3a4a48e62d1ce962125f6511ed
Noskig <48559023+Noskig@users.noreply.github.com>
2022-03-02 17:06:43 +0100

    Added classes, Company Projects and extended User

---
+++ BotheringBugs/Models/Company.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace BotheringBugs.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        [DisplayName("Company Name")]
+        public string Name { get; set; }
+        [DisplayName("Company Description")]
+        public string Description{ get; set; } 
+
+        //Navigation prop
+        public virtual ICollection<BBUser> Members { get; set; }
+        public virtual ICollection<Project> Projects { get; set; }
+    }
+}
---
+++ BotheringBugs/Models/Project.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BotheringBugs.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+
+
+        [DisplayName("Company")]
+        public int CompanyId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [DisplayName("Project Name")]
+        public string Name { get; set; }
+
+        [DisplayName("Description")]
+        public string Description { get; set; }
+
+        [DisplayName("Start Date")]
+        [DataType(DataType.Date)]
+        public DateTimeOffset StartDate { get; set; }
+
+        [DisplayName("End Date")]
+        [DataType(DataType.Date)]
+        public DateTimeOffset EndDate { get; set; }
+
+        [DisplayName("Priority")]
+        public int? ProjectPriorityId { get; set; }
+
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        public IFormFile ImageFormFile { get; set; }
+
+        [DisplayName("File Name")]
+        public string ImageFileName { get; set; }
+        public byte[] ImageFileData { get; set; }
+        [DisplayName("File Extension")]
+        public string ImageFileContentType { get; set; }
+
+
+        [DisplayName("Archived")]
+        public bool Archived { get; set; }
+
+        //Navigation Property
+        public virtual Company Company { get; set; }
+        public virtual ProjectPriority ProjectPriority { get; set; }
+        public virtual ICollection<BBUser> Members { get; set; } = new HashSet<BBUser>();
+        public virtual ICollection<Ticket> Tickets { get; set; } = new HashSet<Ticket>();
+    }
+}
