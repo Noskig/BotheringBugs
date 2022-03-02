7295457b896c6732943689c5bc6b00254b79b97b
Noskig <48559023+Noskig@users.noreply.github.com>
2022-03-02 13:18:24 +0100

    Add project files.

---
+++ BotheringBugs/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BotheringBugs.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BotheringBugs.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<BBUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        
+    }
+}
---
+++ BotheringBugs/Models/BBUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BotheringBugs.Models
+{
+    public class BBUser : IdentityUser
+    {
+        [Required]
+        [Display(Name ="First Name")]
+        [PersonalData]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name ="Last Name")]
+        [PersonalData]
+        public string LastName { get; set; }
+        [NotMapped]
+        [Display(Name = "Full Name")]
+        public string FullName { get { return $"{FirstName} {LastName}";}}
+
+    }
+}
